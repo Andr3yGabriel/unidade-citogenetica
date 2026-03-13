eef88807a6a05b7ea75b57b7137965be02a00fd5
davi.cruz <davi.cruz@mirante.net.br>
2026-03-13 14:53:16 -0300

    fix: ajustando os tipos de exames

--- api-citogenetica/src/seeders/20250625021112-03-tipos-exame.ts
+++ api-citogenetica/src/seeders/20250625021112-03-tipos-exame.ts
@@ -3,11 +3,10 @@ import { QueryInterface } from 'sequelize';
 module.exports = {
   up: async (queryInterface: QueryInterface) => {
     await queryInterface.bulkInsert('tipos_exame', [
-      { nome: 'sangue' },
-      { nome: 'urina' },
-      { nome: 'fezes' },
-      { nome: 'secrecao' },
-      { nome: 'outro' }
+      { nome: 'Cariótipo' },
+      { nome: 'Fish' },
+      { nome: 'frágil' },
+      { nome: 'MLPA' },
     ], {});
   },
 
